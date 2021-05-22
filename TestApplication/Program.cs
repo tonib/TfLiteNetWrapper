42010e97232f3777f587cb11491886d52c87ddb4
tonib <tonib@lsisa.com>
2021-05-22 21:59:49 +0200

    Aded test model. Test output

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TfLiteNetWrapper;
 
 namespace TestApplication
@@ -9,10 +10,42 @@ namespace TestApplication
 		{
 			try
 			{
-				ModelWrapper model = new ModelWrapper(@"D:\kbases\subversion\fuentesTensorflow2.4\pruebasDlls\ConsoleApplication1\Debug\model.tflite", 2);
 
-				Int32[] values = new Int32[64];
-				model.InputTensors[0].SetValues(values);
+				string[] sequenceInputs = {
+					"wordType","keywordIdx","kbObjectTypeIdx","dataTypeIdx","dataTypeExtTypeHash","isCollection","lengthBucket","decimalsBucket","textHash0","textHash1",
+					"textHash2","textHash3","controlType" };
+				HashSet<string> seqColumns = new HashSet<string>(sequenceInputs);
+
+				string[] outputNamesOriginal = { "isCollection", "lengthBucket", "decimalsBucket", "outputTypeIdx", "outputExtTypeHash", "textHash0", "textHash1", "textHash2", 
+					"textHash3", "isControl" };
+				// Output names for TF lite are wrong. They keep a pattern: Order is the same as the original names sorted alphabetically:
+				List<string> outputNames = new List<string>(outputNamesOriginal);
+				outputNames.Sort();
+
+				ModelWrapper model = new ModelWrapper(@"D:\kbases\subversion\TfLiteNetWrapper\TfLiteNetWrapper\TestApplication\model.tflite", 2);
+
+				foreach(TensorWrapper tensor in model.InputTensors)
+				{
+					int dim = tensor.Dimensions[0];
+					Int32[] input = new Int32[dim];
+					for (int i = 0; i < dim; i++)
+						input[i] = -1;
+					if (!seqColumns.Contains(tensor.Name))
+						input[0] = 0;
+					tensor.SetValues(input);
+				}
+
+				model.InvokeInterpreter();
+
+				Dictionary<string, float[]> result = new Dictionary<string, float[]>();
+				for(int i=0; i<model.OutputTensors.Count; i++)
+				{
+					TensorWrapper tensor = model.OutputTensors[i];
+					int dim = tensor.Dimensions[0];
+					float[] output = new float[dim];
+					tensor.GetValues(output);
+					result.Add(outputNames[i], output);
+				}
 
 				Console.WriteLine("Done");
 			}
